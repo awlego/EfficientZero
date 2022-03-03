438576b74874e63f517e7cb9bf7fadb5fddc46da
Alexander Wilson <awlego@gmail.com>
2022-03-02 23:56:48 -0800

    Updating training script to fid my computer's abilities

--- a_train.sh
+++ a_train.sh
@@ -3,9 +3,10 @@ export CUDA_DEVICE_ORDER='PCI_BUS_ID'
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 python main.py --env BreakoutNoFrameskip-v4 --case atari --opr train --force \
-  --num_gpus 4 --num_cpus 96 --cpu_actor 14 --gpu_actor 20 \
+  --num_gpus 1 --num_cpus 10 --cpu_actor 1 --gpu_actor 1 \
   --seed 0 \
   --use_priority \
   --use_max_priority \
   --amp_type 'torch_amp' \
-  --info 'EfficientZero-V1'
+  --info 'EfficientZero-V1' \
+  --object_store_memory 16106127360
